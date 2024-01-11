89b8235a71d67bdeb807e6a09ab0fc5041c897a9
Deven1902 <devenamazingnandapurkar@gmail.com>
2024-01-11 20:47:36 +0530

    added yoga poses json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import PoseDetect from './pages/PoseDetect'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Header from './components/header'
-import Footer from './components/footer'
-import Landing from './pages/Landing'
-import Page_Not_Found from './pages/404'
-import './index.css'
-import { useRef, useState } from 'react'
-import { useEffect } from 'react'
-import PoseDetector from './util/poseDetector'
-import { PoseDetectorContext } from './context/poseDetectorContext'
+import PoseDetect from './pages/PoseDetect';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import Landing from './pages/Landing';
+import Page_Not_Found from './pages/404';
+import './index.css';
+import { useRef, useState } from 'react';
+import { useEffect } from 'react';
+import PoseDetector from './util/poseDetector';
+import { PoseDetectorContext } from './context/poseDetectorContext';
 
 const Layout = () => {
   return (
