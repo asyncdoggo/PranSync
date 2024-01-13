95606b1ea927e6510912d98ccbeba3319fa98ce4
asyncdoggo <ayushdeshpande81@gmail.com>
2024-01-13 19:33:20 +0530

    Add video upload and controls to pose Guide

--- src/pages/PoseGuide.jsx
+++ src/pages/PoseGuide.jsx
@@ -8,7 +8,8 @@ export default function PoseGuide() {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const param = useParams()
-
+    const [isVideo, setIsVideo] = useState(null);
+    const [progress, setProgress] = useState(0);
 
     const constraints = {
         video:
@@ -19,15 +20,29 @@ export default function PoseGuide() {
                 : true
     }
 
+    const videoSeek = (e) => {
+        videoRef.current.currentTime = e.target.value * videoRef.current.duration / 100
+    }
+
+    const videoPause = (e) => {
+        if (videoRef.current.paused) {
+            videoRef.current.play()
+            e.target.innerHTML = "pause_circle"
+        } else {
+            videoRef.current.pause()
+            e.target.innerHTML = "play_circle"
+        }
+    }
+
     const videoLoad = async () => {
         onResize()
-        console.log("video width:", video.videoWidth, "video height:", video.videoHeight);
-        if (video.readyState >= 3) {
+        console.log("video width:", videoRef.current.videoWidth, "video height:", videoRef.current.videoHeight);
+        if (videoRef.current.readyState >= 3) {
 
             setInterval(async () => {
-                if (video.paused || video.ended) return;
-                const poses = await poseDetector.current.getPose(video)
-                poseDetector.current.drawPoses(poses, video);
+                if (videoRef.current.paused || videoRef.current.ended) return;
+                const poses = await poseDetector.current.getPose(videoRef.current)
+                poseDetector.current.drawPoses(poses, videoRef.current);
                 // poseDetector.current.drawSkeleton(poses);
 
                 const anglesDiff = calculateAngleDifference(poses, param.pose)
@@ -41,20 +56,18 @@ export default function PoseGuide() {
         }
     }
 
-    async function detectPoseFromVideo(video) {
-        video.addEventListener('loadeddata', videoLoad);
-    }
 
     async function startVideo() {
         const video = videoRef.current;
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         video.srcObject = stream;
-        detectPoseFromVideo(video);
+        video.addEventListener('loadeddata', videoLoad);
     }
 
     async function stopVideo() {
         videoRef.current.srcObject.getTracks().forEach(track => track.stop())
-        video.removeEventListener('loadeddata', videoLoad)
+        videoRef.current.removeEventListener('loadeddata', videoLoad)
+        videoRef.current.removeEventListener('timeupdate', TimeUpdate)
     }
 
     const onResize = () => {
@@ -74,6 +87,25 @@ export default function PoseGuide() {
 
         poseDetector.current.setVideoData(videoRef.current.videoWidth, videoRef.current.videoHeight, canvasRef, window.innerWidth - 50)
 
+    }
+
+
+    function TimeUpdate(params) {
+        try {
+            setProgress(videoRef.current.currentTime / videoRef.current.duration * 100)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    const videoChange = (e) => {
+        // setVideo(e.target.files[0]);
+        setIsVideo(true)
+        videoRef.current.src = URL.createObjectURL(e.target.files[0]);
+        videoRef.current.addEventListener('loadeddata', videoLoad);
+
+        videoRef.current.addEventListener('timeupdate', TimeUpdate);
     }
 
     useEffect(() => {
@@ -98,9 +130,31 @@ export default function PoseGuide() {
                         <h2 className="text-xl font-bold text-center">{param.pose} pose</h2>
 
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={startVideo}>Start Camera</button>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={stopVideo}>Stop Camera</button>
-
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={stopVideo}>Stop Video</button>
+                        <label htmlFor="fileupload" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+                            Upload Video
+                        </label>
+                        <input
+                            id="fileupload"
+                            type='file'
+                            accept='video/*'
+                            onChange={videoChange}
+                            hidden
+                        />
                         <canvas id="canvas" ref={canvasRef}></canvas>
+
+                        <div className="flex flex-col justify-center items-center">
+                            {
+                                isVideo ? <>
+                                    <div className="buttons">
+                                        <span id="play" className="material-symbols-outlined text-4xl" onClick={videoPause}>pause_circle</span>
+                                    </div>
+                                    <input className="text-4xl w-96" type="range" max="100" value={progress} step="any" onChange={videoSeek}></input>
+                                </> : <></>
+                            }
+                        </div>
+
+
                         <video id="video" ref={videoRef} width="1px" autoPlay muted playsInline className="invisible fixed -z-10"></video>
                     </div >
                 </> : <h1>Loading...</h1>
