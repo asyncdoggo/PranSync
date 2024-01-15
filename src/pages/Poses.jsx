fd34cb5e3279486d9f1d239f9345745511ec7c47
Deven1902 <devenamazingnandapurkar@gmail.com>
2024-01-15 16:30:03 +0530

    Updated reference image for all yoga poses

--- src/pages/Poses.jsx
+++ src/pages/Poses.jsx
@@ -1,79 +1,86 @@
 import { Link } from "react-router-dom"
 import mountain_image from "../assets/mountain.png";
+import tree_image from "../assets/tree.png";
+import warrior1 from "../assets/warrior1.png";
+import warrior2 from "../assets/warrior2.png";
+import warrior3 from "../assets/warrior3.png";
+import triangle from "../assets/triangle.png";
+import halfmoon from "../assets/halfmoon.png";
+import chair from "../assets/chair.png";
+import forwardfold from "../assets/forwardfold.png";
+import downwardDog from "../assets/downwardDog.png";
+import triangleTwist from "../assets/triangleTwist.png";
+import crescentlunge from "../assets/crescentlunge.png";
+import dancerpose from "../assets/dancerpose.png";
 
 export default function Poses() {
     const yogaPoses = [
         {
-            pose: "mountain",
+            pose: "Mountain",
             description: "The foundation of all standing poses, Mountain Pose makes a great a starting position, resting pose, or tool to improve posture.",
             image: mountain_image
         },
         {
-            pose: "tree",
+            pose: "Tree",
             description: "Tree Pose stretches the thighs, groins, torso, and shoulders. It builds strength in the ankles and calves, and tones the abdominal muscles. The pose also helps to remedy flat feet and is therapeutic for sciatica.",
-            image: "tree.png"
+            image: tree_image
         },
         {
-            pose: "warrior 1",
+            pose: "Warrior 1",
             description: "Warrior I is a standing lunge that deeply strengthens your legs and core. It also stretches your chest and shoulders, and opens your hips.",
-            image: "warrior1.png"
+            image: warrior1
         },
         {
             pose: "warrior 2",
             description: "Warrior II strengthens the legs, opens the hips and chest and shoulders, and stretches the groins, ankles and calves.",
-            image: "warrior2.png"
+            image: warrior2
         },
         {
             pose: "warrior 3",
             description: "Warrior III strengthens the legs and ankles, stretches the chest and shoulders, and improves balance and posture.",
-            image: "warrior3.png"
+            image: warrior3
         },
         {
             pose: "triangle",
             description: "Triangle Pose stretches the legs, hips, groin, shoulders, chest, spine and ankles. It also increases both physical and mental stability, and cultivates balance and concentration.",
-            image: "triangle.png"
+            image: triangle
         },
         {
             pose: "half moon",
             description: "Half Moon Pose strengthens the abdomen, ankles, thighs, buttocks, and spine. It stretches the groins, hamstrings and calves, shoulders, chest, and spine.",
-            image: "halfmoon.png"
+            image: halfmoon
         },
         {
             pose: "chair",
             description: "Chair Pose clearly works the muscles of the arms and legs, but it also stimulates the diaphragm and heart.",
-            image: "chair.png"
+            image: chair
         },
         {
             pose: "forward fold",
             description: "Forward Fold stretches the hamstrings, calves, and hips. It also strengthens the thighs and knees, and keeps your spine strong and flexible.",
-            image: "forwardfold.png"
+            image: forwardfold
         },
         {
             pose: "downward dog",
             description: "Downward Dog stretches the shoulders, hamstrings, calves, arches, and hands. It strengthens the arms and legs, and helps relieve fatigue and sciatica.",
-            image: "downwarddog.png"
+            image: downwardDog
         },
         {
-            pose: "Traingle Twist",
+            pose: "Triangle Twist",
             description: "Triangle Twist Pose is a standing yoga pose that tones the legs, reduces stress, and increases stability and balance.",
-            image: "trianglepose.png"
+            image: triangleTwist
         }, 
         {
             pose: "Crescent Lunge",
             description: "Crescent Lunge strengthens and stretches the legs, hips, and thighs, while opening the chest, shoulders, and arms.",
-            image: "crescentlunge.png"
+            image: crescentlunge
         }, 
         {
             pose: "Dancer Pose",
             description: "Dancer Pose stretches the shoulders, chest, thighs, groins, and abdomen. It strengthens the legs and ankles, improves balance, and adds grace and poise to your practice.",
-            image: "dancerpose.png"
-        }, 
-        {
-            pose: "Chair Pose", 
-            description: "Chair Pose clearly works the muscles of the arms and legs, but it also stimulates the diaphragm and heart.",
-            image: "chairpose.png"
-        }, 
-        
+            image: dancerpose
+        },
+
     ]
     
     
