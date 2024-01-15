c2a2f890151bdb0a203518958bb23115d64ae9b0
Deven1902 <devenamazingnandapurkar@gmail.com>
2024-01-15 08:45:05 +0530

    Added hero section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
-
 export default function Navbar({ fixed }) {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
@@ -12,9 +11,10 @@ export default function Navbar({ fixed }) {
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                         <Link
                             className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                            style={{ textTransform: "capitalize" }}
                             href="#pablo"
                         >
-                            Yoga
+                            Sync
                         </Link>
                         <button
                             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,37 @@
+// import React from 'react';
+
 export default function Landing() {
     return (
-        <div className='w-full'>
-            Me is the landing
+        // <div className='w-full'>
+        //     Me is the landing
+        // </div> 
+        
+        // <div className='flex flex-col items-center justify-center h-screen bg-green-500'>
+        //     <h1 className='text-5xl text-white font-bold'>Welcome to PranaSync</h1>
+        //     <p className='text-2xl text-white mt-4'>Your syncing partner for a healthy life style</p>
+        //     <button className='mt-8 px-4 py-2 bg-white text-green-500 rounded-full font-bold text-xl hover:bg-green-200 transition-colors duration-300'>Start Session</button>
+        // </div> 
+
+
+        <div className='flex h-screen bg-green-500'>
+            <div className='w-1/2 flex items-center justify-center'>
+                <div>
+                    <h1 className='text-5xl text-white font-bold'>Welcome to PranaSync</h1>
+                    <p className='text-2xl text-white mt-4'>Breathe. Move. Thrive.</p> 
+                    <p className='text-2xl text-white mt-4'>Experience Harmony with PranaSync.</p>
+
+                    <div className='mt-8'>
+                        <button className='px-4 py-2 bg-white text-green-500 rounded-full font-bold text-xl hover:bg-green-200 transition-colors duration-300'>Get Started</button>
+                    </div>
+                    <div className='mt-4'>
+                        <button className='px-4 py-2 bg-white text-green-500 rounded-full font-bold text-xl hover:bg-green-200 transition-colors duration-300'>Pre-register for Mobile App</button>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className='w-1/2'>
+                <img src='/prana-1.png' alt='Yoga' className='h-full w-full object-cover'/>
+            </div>
         </div>
     )
 }
